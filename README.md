31154ef02554ce7d49c58bfc3288aa38cfc8cef2
Jacky Lin <mr7.jacky@gmail.com>
2021-09-19 21:22:52 -0400

    add parser and options for cmd line

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # meeting_scribe
+
+## Introduction
+
 Tranfer audio into text based on Speech Recognition library.
+We employ the functionality of [Speech Recognition Library](https://pypi.org/project/SpeechRecognition/) to online audio aqcuisition. Potentail application scenes including:
+
+* Meeting Note Taker
+* Online Video Caption Geneartor
+* Video Caption Generator
+
+## Usage
+
+Test on Ubantu 20.04
+
+1. Install `pavucontrol`
+
+```bash
+sudo apt intall pavucontrol
+```
+
+This application allows we to monitor the output of system sound through our python application
+
+2. Install `Speech Recognition` through pip
+
+```bash
+pip install SpeechRecognition
+```
+
+3. Run `speech_recog.py`
+
+```bash
+python speech_recog.py [options]
+```
+
+e.g.
+```bash
+python speech_recog.py --tout 10 --tgen 30
+```
+
+Options:
+
+| Name  | Type  | Default | Description |
+| :--- | :--- | :------ | :- |
+| `-h`/`--help` |    |     | Help info|
+| `--outf`| str| "output.wav" |Output filename for recorded audio|
+|`--tout`| int| 5 |Waiting time for the recorder to stop listening|
+|`--tgen`| int| 10 |Time gap between two consecutive generation of text|
+| `--lang`| str| "en-US" |Audio language|
+
+1. End recording, type `Ctrl+C`, the program will go to end state.
+
+## Dependencies
+
+1. [PyAudio](https://pypi.org/project/PyAudio/)
+2. [Speech Recognition Library](https://pypi.org/project/SpeechRecognition/)
+3. [PulseAudio Volume Control](https://freedesktop.org/software/pulseaudio/pavucontrol/)
--- speech_recog.py
+++ speech_recog.py
@@ -1,49 +1,80 @@
+# Argument handler
+from os import error
+import sys
+import argparse
 # Speech recognition library
 import speech_recognition as sr
+# Signal handler
 import signal
 # Recording using pyaudio
 import pyaudio
 import wave
+
+
 # Parameters
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
+# speech recognition library use single channel 
 CHANNELS = 1
+# Default rate
 RATE = 44100
 RECORD_SECONDS = 15
-WAVE_OUTPUT_FILENAME = "output.wav"
 
 # Signal use to stop the infinite loop
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
-signal.signal(signal.SIGINT, signal_handler)
+def record_handler(output_file_name = "output.wav", timeout = 5, time_per_generate = 10, src_lang = "en-US"):
+    # Check output file name validation
+    if not isinstance(output_file_name, str):
+        error("[Error] Wrong type")
+        sys.exit()
+    elif output_file_name.endswith(".wav"):
+        error("[Error] Unsupported Output Type") 
+        sys.exit()
+    # Control if the file is f
+    signal.signal(signal.SIGINT, signal_handler)
 
-r = sr.Recognizer()
+    r = sr.Recognizer()
+    print("Recording")
 
-print("Recording")
+    interrupted = False
+    frame = []
 
-interrupted = False
-frame = []
+    #TODO-Multi thread for recognition
+    with sr.Microphone() as source:
+        while True:
+            try:
+                audio = r.listen(source, timeout=timeout, phrase_time_limit=time_per_generate)
+                frame.append(audio.get_raw_data())
+                print(r.recognize_google(audio, language=src_lang))
+            except:
+                print("failed")
+            if interrupted:
+                print("Gotta go")
+                break
 
-#TODO-Multi thread for recognition
-with sr.Microphone() as source:
-    while True:
+    # Save recorded audio as file.
+    wf = wave.open(output_file_name, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.sample_width)
+    wf.setframerate(audio.sample_rate)
+    wf.writeframes(b''.join(frame))
+    wf.close()
 
-        try:
-            audio = r.listen(source, timeout=5, phrase_time_limit=1)
-            frame.append(audio.get_raw_data())
-            print(r.recognize_google(audio, language="en-US"))
-        except:
-            print("failed")
-        if interrupted:
-            print("Gotta go")
-            break
+if __name__=="__main__":
+    # Create options for command line inputs
+    parser = argparse.ArgumentParser(description='System output speech audio to text')
+    parser.add_argument('outf', type=str, default="output.wav", help='Output filename for recorded audio')
+    parser.add_argument('tout', type=int, default=5, help='Waiting time for the recorder to stop listening')
+    parser.add_argument('tgen', type=int, default=10, help='Time gap between two consecutive generation of text')
+    parser.add_argument('lang', type=str, default="en-US", help='Audio language')
+    namespace = parser.parse_args()
+
+    record_handler(output_file_name = namespace.outf, 
+                    timeout = namespace.tout, 
+                    time_per_generate = namespace.tgen, 
+                    src_lang = namespace.lang)
 
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(audio.sample_width)
-wf.setframerate(audio.sample_rate)
-wf.writeframes(b''.join(frame))
-wf.close()
